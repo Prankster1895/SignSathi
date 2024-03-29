8fcf8bec485eb15670857d07ed6197758d8f7070
Dyuti Dasgupta <127132215+Prankster1895@users.noreply.github.com>
2024-03-30 00:23:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-const connect=mongoose.connect("mongodb://localhost:27017/SignSathiRegistration")
+const connect=mongoose.connect(process.env.MONGO_URL)
 connect.then(()=>{
 
     console.log("Database connected successfully")
